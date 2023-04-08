99c6ec9b50693c66c8b273ab9a96baf2902f509b
syzhao53 <syzhao@sas.upenn.edu>
2023-04-08 14:38:22 -0400

    ingredients (columns version)

--- src/components/PieDough.js
+++ src/components/PieDough.js
@@ -1,5 +1,9 @@
 import { Checkbox } from '@/components/Checkbox'
-import { Heading, SubHeading, IngredientSubtext } from '@/components/styles/Text'
+import {
+  Heading,
+  SubHeading,
+  IngredientSubtext,
+} from '@/components/styles/Text'
 import { IngredientInfo } from '@/components/styles/IngredientInfo'
 import Ingredients from '@/components/Accordion'
 import { ClockIcon } from '@primer/octicons-react'
@@ -23,27 +27,38 @@ const PieDough = ({ timer }) => {
           </div>
         </div>
         <div className="flex">
+          <div className="w-1/2 pr-20 mr-6">
+            <SubHeading>Ingredients</SubHeading>
+            <Checkbox>3 cups all-purpose flour</Checkbox>
+            <Checkbox>4 tablespoons sugar</Checkbox>
+            <Checkbox>1 teaspoon salt</Checkbox>
+            <Checkbox>2 sticks of butter, cut into small pieces</Checkbox>
+            <Checkbox>1/2 cup ice water</Checkbox>
+            <div className="mt-14">
+              <SubHeading>Notes</SubHeading>
+              <span className="pr-20">
+                Use cold butter and ice water. Need to bring pie plate and fruit
+                peeler.
+              </span>
+              <img
+                className="rounded-md mt-8"
+                src="/pie-dough.png"
+                alt="Pie dough"
+              />
+            </div>
+          </div>
           <div className="w-1/2">
             <SubHeading>Steps</SubHeading>
             <div style={{ overflow: 'visible' }}>
               <Checkbox step>
                 1. Sift flour, sugar, and salt togther into a large bowl.
               </Checkbox>
-              <IngredientSubtext
-                ingredientsList={[
-                  '3 cups all-purpose flour',
-                  '4 tbsp sugar',
-                  '1 tsp salt',
-                ]}
-              />{' '}
               <Checkbox step>
                 2. Add cold butter to flour until clumps form.
               </Checkbox>
-              <IngredientSubtext ingredientsList={['2 sticks butter']} />
               <Checkbox step>
                 3. Sprinkle ice water in dough if necessary.
               </Checkbox>
-              <IngredientSubtext ingredientsList={['1/2 cup ice water']} />
               <Checkbox step>4. Chill dough for 30 minutes.</Checkbox>
               {!hasStarted ? (
                 <button
@@ -62,22 +77,6 @@ const PieDough = ({ timer }) => {
               </Checkbox>
             </div>
           </div>
-          <div className="w-1/2">
-            <img
-              className="rounded-md float-right"
-              src="/pie-dough.png"
-              alt="Pie dough"
-            />
-          </div>
-        </div>
-      </div>
-      <div className="flex">
-        <div className="mt-2">
-          <SubHeading>Notes</SubHeading>
-          <span>
-            Use cold butter and ice water. Need to bring pie plate and fruit
-            peeler.
-          </span>
         </div>
       </div>
     </>
